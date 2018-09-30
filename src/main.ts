ea818b697360cf0d860b8174dc57a3d3034889b5
Son Nguyen <anh.s.nguyen@gmail.com>
2018-09-30 14:50:42 +1000

    Log what is given so we can check what we get

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import * as process from "process"
+import * as fs from "fs"
+import * as console from "console"
 
+const output = fs.createWriteStream('~/logs/plex/stdout.log');
+const errorOutput = fs.createWriteStream('~/logs/plex/stderr.log');
+// custom simple logger
+const logger = new console.Console(output, errorOutput)
 
 process.argv.forEach((val: string, index: number) => {
-  console.log(`${index}: ${val}`);
+  logger.log(`${index}: ${val}`);
 });
